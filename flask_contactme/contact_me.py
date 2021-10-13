21c99b43b82097952edfed812fe7e15c6ea90666
Nigel Sebastian <nigelsebastian21@gmail.com>
2021-10-13 16:47:03 +0800

    10_13_2021

--- flask_contactme/contact_me.py
+++ flask_contactme/contact_me.py
@@ -1,11 +1,10 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
-@app.route('/')
 
-def Contactme():
-    title = "Contact me"
-    return render_template ("flask_contactme.html")
-    
+@app.route('/')
+def main():
+    return render_template("flask_contactme.html")
+
 if __name__ == '__main__':
     app.run()
