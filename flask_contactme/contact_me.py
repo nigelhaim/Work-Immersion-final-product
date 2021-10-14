a9408696af3eead22b9803e737c24ecf29591e8c
Nigel Sebastian <nigelsebastian21@gmail.com>
2021-10-14 09:15:43 +0800

    10_14_2021

--- flask_contactme/contact_me.py
+++ flask_contactme/contact_me.py
@@ -1,10 +1,34 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request
+import smtplib #Import smtp library
+from flask_mail import Mail, Message#CS50 flask import
+ 
+from flask.wrappers import Request
+import os
 app = Flask(__name__)
 
+
+app.config["MAIL_DEFAULT_SENDER"] = os.getenv("MAIL_DEFAULT_SENDER")
+app.config["MAIL_PASSWORD"] = os.getenv("UNCRACKABLE_PASS")
+app.config["MAIL_PORT"] = 587
+app.config["MAIL_SERVER"] = "smtp.gmail.com"
+app.config["MAIL_USE_TLS"] = True
+app.config["MAIL_USERNAME"] = os.getenv("UNCRACKABLE_EMAIL")
+
+mail = Mail(app)
 @app.route('/')
 def main():
     return render_template("flask_contactme.html")
 
+
+@app.route('/form', methods="[POST]")
+def form():
+    firstname = Request.form.get("First_name")
+    lastname = Request.form.get("Last_name")
+    email = Request.form.get("Email")
+
+    msg = Request.form.get("msg")
+    myemail = "UNCRACKABLE_EMAIL"
+    message = Message("NEW MESSAGE!", recipients=[myemail])
+    mail.send(message)
 if __name__ == '__main__':
     app.run()
