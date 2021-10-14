48164daffbd9b2335d1bf05105b82e1b07f37851
Nigel Sebastian <nigelsebastian21@gmail.com>
2021-10-14 13:51:05 +0800

    10_14_2021

--- flask_contactme/contact_me.py
+++ flask_contactme/contact_me.py
@@ -4,10 +4,9 @@ import os
 
 
 app = Flask(__name__)
-
-e_user = os.environ.get("ENCRYPT_USER")
-e_pass = os.environ.get("ENCRYPT_PASS")
-to_user = os.environ.get("TO_USER")
+e_user = os.environ.get('ENCYPT_USER')
+e_pass = os.environ.get('ENCRYPT_PASS')
+to_user = os.environ.get('TO_USER')
 
 @app.route('/')
 def main():
@@ -19,14 +18,12 @@ def form():
     firstname = request.form.get("First_name")
     lastname = request.form.get("Last_name")
     email = request.form.get("Email")
-
+    space = " "
     mess = request.form["message_1"]
-
-    msg = ("NEW MESSAGE! /n")
     server = smtplib.SMTP("smtp.gmail.com", 587)
     server.starttls()
     server.login(e_user, e_pass)
-    server.sendmail(e_user, to_user, "NEW MESSAGE! /n")
+    server.sendmail(e_user, to_user, ("New message!\n" + "From: " + firstname + " " + lastname + "\n"+ "Email:" + email + "\n" + "Message:\n" + "\n" + mess ))
     
     title ="Thank you!"
     return render_template("Submission.html", title = title)
